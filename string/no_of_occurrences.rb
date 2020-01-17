aa06568576fe77ba90aec82ce0bfccb3d85d7b86
Gokul <pgokulmca@gmail.com>
2020-01-18 01:19:02 +0530

    find number of occurrences form string

---
+++ string/no_of_occurrences.rb
@@ -0,0 +1,10 @@
+# Write a program to count the number of occurrences of a character in String
+
+def result(str:, looking_char: '')
+	str.count(looking_char)
+end
+
+
+str = 'this is a test from gklsan'
+
+result(str: str, looking_char: 't')
