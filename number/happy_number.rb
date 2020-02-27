eb1a8fa62840f83ad44f28e708835a8452fbbc50
Gokul <pgokulmca@gmail.com>
2020-02-27 22:39:45 +0530

    number happy number 3rd solution has been added

--- number/happy_number.rb
+++ number/happy_number.rb
@@ -13,7 +13,7 @@
 # 62 + 82 = 100
 # 12 + 02 + 02 = 1
 
-
+# Solution 1
 def is_happy_1?(n, sums = [])
     return false if n.to_i < 1 || sums.include?(n)
     return true if n == 1
@@ -23,6 +23,8 @@ def is_happy_1?(n, sums = [])
     is_happy_1?(total, sums)
 end
 
+
+# Solution 2
 def is_happy_2?(n)
   seen = []
   while n != 1 && !seen.include?(n)
@@ -32,6 +34,30 @@ def is_happy_2?(n)
   n == 1
 end
 
+# Solution 3
+def sum_square_digits(n)
+    sum = 0
+    while(n > 0)
+        digit = n % 10
+        sum += digit * digit
+        n = (n - digit) / 10
+    end
+    
+    sum
+end
+
+def is_happy?(n)
+    sum = n
+    unhappy_num = 4
+
+    while(true)
+        sum = sum_square_digits(sum)
+        return true if sum == 1
+        return false if sum == unhappy_num
+    end
+end
+
 
 puts is_happy_1?(19)
 puts is_happy_1?(19)
+puts is_happy?(19)
