66630793db3deb4661a363f8f805a7ddb8abedce
Gokul <pgokulmca@gmail.com>
2020-03-19 22:33:37 +0530

    group anagrams questions has been added.

---
+++ array/group_anagrams.rb
@@ -0,0 +1,24 @@
+# Given an array of strings, group anagrams together.
+
+# Example:
+
+# Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+# Output:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
+# ]
+# Note:
+
+# All inputs will be in lowercase.
+# The order of your output does not matter.
+
+
+def group_anagrams(strs)
+    
+end
+
+
+puts group_anagrams ["eat", "tea", "tan", "ate", "nat", "bat"]
+
